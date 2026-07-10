93068e5a7415685e6631043852c07c5a7a2a15ec
danielosorio <256449502+santiagoosorio0708e-wq@users.noreply.github.com>
2026-07-10 08:37:22 -0500

    refactor: actualizar triggers.sql y funciones.sql sin comentarios

--- triggers.sql
+++ triggers.sql
@@ -2,20 +2,16 @@ USE pizzeria_don_piccolo;
 
 DELIMITER //
 
--- Trigger para actualizar stock de ingredientes al insertar un pedido
 CREATE TRIGGER tr_actualizar_stock_ingredientes
 AFTER INSERT ON pedido_pizzas
 FOR EACH ROW
 BEGIN
-    -- Se reduce el stock de los ingredientes que componen la pizza
-    -- multiplicando por la cantidad de pizzas solicitadas
     UPDATE ingredientes i
     INNER JOIN pizza_ingredientes pi ON i.id_ingrediente = pi.id_ingrediente
     SET i.stock = i.stock - (pi.cantidad * NEW.cantidad)
     WHERE pi.id_pizza = NEW.id_pizza;
 END //
 
--- Trigger de auditoría para cambios de precio en pizzas
 CREATE TRIGGER tr_auditoria_precio_pizza
 AFTER UPDATE ON pizzas
 FOR EACH ROW
@@ -26,12 +22,10 @@ BEGIN
     END IF;
 END //
 
--- Trigger para liberar al repartidor cuando termina el domicilio
 CREATE TRIGGER tr_liberar_repartidor
 AFTER UPDATE ON domicilios
 FOR EACH ROW
 BEGIN
-    -- Si se registró la hora de entrega y antes estaba vacía
     IF OLD.hora_entrega IS NULL AND NEW.hora_entrega IS NOT NULL THEN
         UPDATE repartidores
         SET estado = 'disponible'
